18558115a586505b4fee3b6c8e7ac0e14a3cefb2
Kristina <kristina.dudnyk20@gmail.com>
2023-07-14 10:43:39 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [quotesData, setQuotesData] = useState(null);
+
+  useEffect(() => {
+    const fetchQuotesData = async (url) => {
+      try {
+        setIsLoading(true);
+        setIsError(false);
+
+        const response = await fetch(url);
+        console.log("fetchQuotesData response", response);
+
+        if (!response.ok) {
+          throw new Error("fetch failed");
+        }
+
+        const json = await response.json();
+        console.log("fetchQuotesData json", json);
+
+        setQuotesData(json);
+      } catch (error) {
+        console.log("fetchQuotesData error block:", error);
+        setIsError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchQuotesData("https://kristina-quote-server.onrender.com/quotes/");
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoading && <div>Loading..</div>}
+      {isError && <div>Error: {isError}</div>}
+      {!!quotesData && (
+        <main>
+          <h1 className="app-title">Quotes App</h1>
+          <div className="quote-cards-container">
+            {quotesData.map(({ id, author, quote }) => {
+              return (
+                <div key={id} className="quote-card">
+                  <p className="quote-author">{author}</p>
+                  <p className="quote-text">{`"${quote}"`}</p>
+                </div>
+              );
+            })}
+          </div>
+        </main>
+      )}
     </div>
   );
-}
+};
 
 export default App;
